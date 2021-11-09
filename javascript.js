19606e4a3468e73778701be5abac06446dac0c0a
SoulCartel117 <jeremiahmartin117@gmail.com>
2021-11-09 15:29:23 -0500

    added a js file with functions

---
+++ javascript.js
@@ -0,0 +1,29 @@
+
+let computerOptions = ["rock","paper","scissors"];
+
+function d20 (computeroptions) { /// returns random  choice
+    return computerChoice = computerOptions[Math.floor((Math.random()*computeroptions.length))];
+  
+};
+
+function compare(){
+    if(userChoice === computerChoice){
+        console.log(tie)
+    }
+    else if (userChoice === "rock"){
+        if (computerChoice === "paper"){
+            console.log("Rock wins")
+        }
+    }
+    else if (userChoice === "paper"){
+        if (computerChoice === "rock"){
+            console.log("Paper wins")
+        }
+    }
+     else if (userChoice === "scissors"){
+         if (computerChoice === "paper"){
+             console.log("Scissors wins")
+         }
+     }   
+    }
+}   
